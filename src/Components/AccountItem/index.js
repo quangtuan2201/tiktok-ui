0cdb174c689ef5be291d5151c9fb035192508e51
TuanAnh <nnguyentuananh2201@gmail.com>
2024-04-10 21:00:15 +0700

    Xay dung layout Header #2

---
+++ src/Components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { memo } from 'react';
+const cx = classNames.bind(styles);
+function AccountItem({ userInfo }) {
+    // console.log('UserInfo: ', userInfo);
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                style={{ objectFit: 'cover' }}
+                className={cx('avatar')}
+                src={require('~/assets/images/avatar.jpeg')}
+                alt="Huế"
+            ></img>
+            <div className={cx('info')}>
+                <h4 className={'name'}>
+                    <span>Nguyễn Văn A </span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+export default memo(AccountItem);
